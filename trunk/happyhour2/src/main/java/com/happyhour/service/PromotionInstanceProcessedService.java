750c705e0e187f38f888df8ace17801c97ade11f
matiaskochman@gmail.com <matiaskochman@gmail.com@d9b698d8-394f-40b8-1260-0c40281b2d4f>
2014-01-13 12:43:36 +0000

    promotionInstanceProcessed y promotionRequestProcessed crud done

---
+++ trunk/happyhour2/src/main/java/com/happyhour/service/PromotionInstanceProcessedService.java
@@ -0,0 +1,18 @@
+package com.happyhour.service;
+import java.util.List;
+
+import org.springframework.roo.addon.layers.service.RooService;
+
+import com.happyhour.entity.PromotionInstanceProcessed;
+
+@RooService(domainTypes = { com.happyhour.entity.PromotionInstanceProcessed.class })
+public interface PromotionInstanceProcessedService {
+    public abstract long countAllPromotionInstanceProcesseds();    
+    public abstract void deletePromotionInstanceProcessed(PromotionInstanceProcessed promotionInstanceProcessed);    
+    public abstract PromotionInstanceProcessed findPromotionInstanceProcessed(Long id);    
+    public abstract List<PromotionInstanceProcessed> findAllPromotionInstanceProcesseds();    
+    public abstract List<PromotionInstanceProcessed> findPromotionInstanceProcessedEntries(int firstResult, int maxResults);    
+    public abstract void savePromotionInstanceProcessed(PromotionInstanceProcessed promotionInstanceProcessed);    
+    public abstract PromotionInstanceProcessed updatePromotionInstanceProcessed(PromotionInstanceProcessed promotionInstanceProcessed);    
+	
+}
