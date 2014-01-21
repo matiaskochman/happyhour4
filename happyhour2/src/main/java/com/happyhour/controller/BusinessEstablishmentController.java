40d456c0c0e299daa64a93a9b7fd191ac457b526
matiaskochman@gmail.com <matiaskochman@gmail.com@d9b698d8-394f-40b8-1260-0c40281b2d4f>
2014-01-21 12:52:40 +0000

    deleting aspectJ controllers

--- happyhour2/src/main/java/com/happyhour/controller/BusinessEstablishmentController.java
+++ happyhour2/src/main/java/com/happyhour/controller/BusinessEstablishmentController.java
@@ -1,21 +1,28 @@
 package com.happyhour.controller;
 import java.io.UnsupportedEncodingException;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.roo.addon.web.mvc.controller.json.RooWebJson;
 import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.util.UriUtils;
 import org.springframework.web.util.WebUtils;
+
 import com.happyhour.entity.BusinessEstablishment;
 import com.happyhour.service.BusinessEstablishmentService;
 import com.happyhour.service.PromotionInstanceService;
@@ -115,4 +122,68 @@ public class BusinessEstablishmentController {
         }
         return pathSegment;
     }
+    
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, headers = "Accept=application/json")
+    @ResponseBody
+    public ResponseEntity<String> showJson(@PathVariable("id") Long id) {
+        BusinessEstablishment businessEstablishment = businessEstablishmentService.findBusinessEstablishment(id);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json; charset=utf-8");
+        if (businessEstablishment == null) {
+            return new ResponseEntity<String>(headers, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<String>(businessEstablishment.toJson(), headers, HttpStatus.OK);
+    }
+    
+    @RequestMapping(headers = "Accept=application/json")
+    @ResponseBody
+    public ResponseEntity<String> listJson() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json; charset=utf-8");
+        List<BusinessEstablishment> result = businessEstablishmentService.findAllBusinessEstablishments();
+        return new ResponseEntity<String>(BusinessEstablishment.toJsonArray(result), headers, HttpStatus.OK);
+    }
+    
+    @RequestMapping(method = RequestMethod.POST, headers = "Accept=application/json")
+    public ResponseEntity<String> createFromJson(@RequestBody String json) {
+        BusinessEstablishment businessEstablishment = BusinessEstablishment.fromJsonToBusinessEstablishment(json);
+        businessEstablishmentService.saveBusinessEstablishment(businessEstablishment);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+        return new ResponseEntity<String>(headers, HttpStatus.CREATED);
+    }
+    
+    @RequestMapping(value = "/jsonArray", method = RequestMethod.POST, headers = "Accept=application/json")
+    public ResponseEntity<String> createFromJsonArray(@RequestBody String json) {
+        for (BusinessEstablishment businessEstablishment: BusinessEstablishment.fromJsonArrayToBusinessEstablishments(json)) {
+            businessEstablishmentService.saveBusinessEstablishment(businessEstablishment);
+        }
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+        return new ResponseEntity<String>(headers, HttpStatus.CREATED);
+    }
+    
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT, headers = "Accept=application/json")
+    public ResponseEntity<String> updateFromJson(@RequestBody String json, @PathVariable("id") Long id) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+        BusinessEstablishment businessEstablishment = BusinessEstablishment.fromJsonToBusinessEstablishment(json);
+        if (businessEstablishmentService.updateBusinessEstablishment(businessEstablishment) == null) {
+            return new ResponseEntity<String>(headers, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<String>(headers, HttpStatus.OK);
+    }
+    
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, headers = "Accept=application/json")
+    public ResponseEntity<String> deleteFromJson(@PathVariable("id") Long id) {
+        BusinessEstablishment businessEstablishment = businessEstablishmentService.findBusinessEstablishment(id);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+        if (businessEstablishment == null) {
+            return new ResponseEntity<String>(headers, HttpStatus.NOT_FOUND);
+        }
+        businessEstablishmentService.deleteBusinessEstablishment(businessEstablishment);
+        return new ResponseEntity<String>(headers, HttpStatus.OK);
+    }
+    
 }
--- happyhour2/src/main/java/com/happyhour/controller/PromotionInstanceController.java
+++ happyhour2/src/main/java/com/happyhour/controller/PromotionInstanceController.java
@@ -1,21 +1,30 @@
 package com.happyhour.controller;
 import java.io.UnsupportedEncodingException;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+
 import org.joda.time.format.DateTimeFormat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.roo.addon.web.mvc.controller.json.RooWebJson;
 import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.util.UriUtils;
 import org.springframework.web.util.WebUtils;
+
 import com.happyhour.entity.PromotionInstance;
 import com.happyhour.service.BusinessEstablishmentService;
 import com.happyhour.service.PromotionDescriptionService;
@@ -131,4 +140,68 @@ public class PromotionInstanceController {
         }
         return pathSegment;
     }
+    
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, headers = "Accept=application/json")
+    @ResponseBody
+    public ResponseEntity<String> showJson(@PathVariable("id") Long id) {
+        PromotionInstance promotionInstance = promotionInstanceService.findPromotionInstance(id);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json; charset=utf-8");
+        if (promotionInstance == null) {
+            return new ResponseEntity<String>(headers, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<String>(promotionInstance.toJson(), headers, HttpStatus.OK);
+    }
+    
+    @RequestMapping(headers = "Accept=application/json")
+    @ResponseBody
+    public ResponseEntity<String> listJson() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json; charset=utf-8");
+        List<PromotionInstance> result = promotionInstanceService.findAllPromotionInstances();
+        return new ResponseEntity<String>(PromotionInstance.toJsonArray(result), headers, HttpStatus.OK);
+    }
+    
+    @RequestMapping(method = RequestMethod.POST, headers = "Accept=application/json")
+    public ResponseEntity<String> createFromJson(@RequestBody String json) {
+        PromotionInstance promotionInstance = PromotionInstance.fromJsonToPromotionInstance(json);
+        promotionInstanceService.savePromotionInstance(promotionInstance);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+        return new ResponseEntity<String>(headers, HttpStatus.CREATED);
+    }
+    
+    @RequestMapping(value = "/jsonArray", method = RequestMethod.POST, headers = "Accept=application/json")
+    public ResponseEntity<String> createFromJsonArray(@RequestBody String json) {
+        for (PromotionInstance promotionInstance: PromotionInstance.fromJsonArrayToPromotionInstances(json)) {
+            promotionInstanceService.savePromotionInstance(promotionInstance);
+        }
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+        return new ResponseEntity<String>(headers, HttpStatus.CREATED);
+    }
+    
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT, headers = "Accept=application/json")
+    public ResponseEntity<String> updateFromJson(@RequestBody String json, @PathVariable("id") Long id) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+        PromotionInstance promotionInstance = PromotionInstance.fromJsonToPromotionInstance(json);
+        if (promotionInstanceService.updatePromotionInstance(promotionInstance) == null) {
+            return new ResponseEntity<String>(headers, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<String>(headers, HttpStatus.OK);
+    }
+    
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, headers = "Accept=application/json")
+    public ResponseEntity<String> deleteFromJson(@PathVariable("id") Long id) {
+        PromotionInstance promotionInstance = promotionInstanceService.findPromotionInstance(id);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+        if (promotionInstance == null) {
+            return new ResponseEntity<String>(headers, HttpStatus.NOT_FOUND);
+        }
+        promotionInstanceService.deletePromotionInstance(promotionInstance);
+        return new ResponseEntity<String>(headers, HttpStatus.OK);
+    }
+    
 }
