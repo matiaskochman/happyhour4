40acb2d19cf5c67adc03fbf4dfadac5cada83cc9
matiaskochman@gmail.com <matiaskochman@gmail.com@d9b698d8-394f-40b8-1260-0c40281b2d4f>
2014-02-21 07:48:19 +0000

    refactor date format and cleaning code

--- trunk/happyhour2/src/main/java/com/happyhour/service/UsuarioServiceImpl.java
+++ trunk/happyhour2/src/main/java/com/happyhour/service/UsuarioServiceImpl.java
@@ -10,11 +10,13 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.happyhour.entity.Authority;
 import com.happyhour.entity.Usuario;
 
 @Service(value="usuarioService")
+@Transactional
 public class UsuarioServiceImpl implements UsuarioService ,UserDetailsService{
 
 	public UserDetails loadUserByUsername(String username)
